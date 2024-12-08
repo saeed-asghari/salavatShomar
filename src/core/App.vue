27a0b3cd0fb364f23db1f9800e9cb42ae239af02
saeed <saeed.asghari.241@gmail.com>
2024-12-08 22:06:16 +0330

    add period

--- src/core/App.vue
+++ src/core/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="touchable-area"  @mouseup="handleTouchEnd" @mouseleave="handleTouchEnd"
-    @touchstart="handleTouchStart" @touchend="handleTouchEnd">
+  <div class="touchable-area" @mouseup="handleTouchEnd" @mouseleave="handleTouchEnd" @touchstart="handleTouchStart"
+  @touchend="handleTouchEnd">
+  <div>تعداد دور :{{ period }}</div>
     <p class="counter">صلوات : {{ counter }}</p>
     <button class="reset-button" @click="resetCounter">پاک کردن</button>
   </div>
@@ -12,9 +13,13 @@ import { ref, onMounted } from "vue";
 
 const counter = ref(0);
 const isPressed = ref(false);
+const period = ref(0);
 const handleTouchStart = () => {
   isPressed.value = true;
   counter.value++;
+  if (counter.value % 100 === 0) {
+    period.value++;
+  }
 };
 
 const handleTouchEnd = () => {
@@ -50,10 +55,13 @@ onMounted(() => {
   transition: background-color 0.2s ease, transform 0.1s ease;
   user-select: none;
   touch-action: manipulation;
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 
 .touchable-area:active {
-  background-color: #e0e0e0;
+  background-color: #f0f0f0;
   transform: scale(0.98);
 }
 
