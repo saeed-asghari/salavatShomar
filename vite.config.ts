cb3dec301d484f9679114074fb1d9f5531d3a768
saeed <saeed.asghari.241@gmail.com>
2024-12-09 20:25:28 +0330

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,41 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'صلوات شمار',
+        short_name: 'salavat',
+        description: 'صلوات شمار',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  
   ],
   base: "/",
   resolve: {
@@ -24,4 +53,5 @@ export default defineConfig({
       'X-Content-Type-Options': 'nosniff',
     },
   },
+  
 })
